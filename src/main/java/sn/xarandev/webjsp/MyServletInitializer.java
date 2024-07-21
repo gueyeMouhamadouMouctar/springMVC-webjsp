bfc7ae5774226e656735245d08fe6829e1bb2de5
gueyeMouhamadouMouctar <mouhamadoumouctargueye@gmail.com>
2024-07-21 00:07:02 -0400

    first commit

---
+++ src/main/java/sn/xarandev/webjsp/MyServletInitializer.java
@@ -0,0 +1,26 @@
+package sn.xarandev.webjsp;
+
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import sn.xarandev.webjsp.config.SpringWebConfig;
+
+public class MyServletInitializer
+        extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    // services and data sources
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    // controller, view resolver, handler mapping
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringWebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: mouha
+  Date: 20/07/2024
+  Time: 15:42
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>Welcome</h1>
+</body>
+</html>
